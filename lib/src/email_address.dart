7f4f8458d4c6474140e4a2e16ad359fc02f46938
Robert Virkus <Robert.Virkus@enough.de>
2020-02-04 23:50:40 +0100

    remove unused source file

--- lib/src/email_address.dart
+++
@@ -1,9 +0,0 @@
-class EmailAddress {
-  String name;
-  String email;
-  String get domain =>
-      email.contains('@') ? email.substring(email.indexOf('@') + 1) : null;
-
-  EmailAddress(this.name, this.email);
-  EmailAddress.empty();
-}
